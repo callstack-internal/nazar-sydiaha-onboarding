b569537c47abc4925107cfbf4b0e5fd64f3d186a
Nazar Sydiaha <nazar.sydiaha@callstack.com>
2025-08-22 12:23:25 +0200

    feat: initial project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+      },
+    ],
+  ],
 };
